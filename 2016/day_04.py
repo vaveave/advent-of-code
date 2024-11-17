99d43dba809e0099939f24e3cb5280d53b10b46b
Valerio Avenoso <valerio.avenoso@gmail.com>
2024-11-17 11:03:16 +0100

    complete year 2016 - day 4 - part 1

--- 2016/day_04.py
+++ 2016/day_04.py
@@ -19,9 +19,27 @@ def part1(input_data):
     return str(real_rooms_counter)
 
 
+def decrypt_room_name(room_name, key):
+    decrypted_name = ""
+    for ch in room_name:
+        if ch != "-":
+            decrypted_name += chr((ord(ch) - 97 + key) % 26 + 97)
+        else:
+            decrypted_name += "-"
+    return decrypted_name
+
+
 def part2(input_data):
-    # Implement part 2 solution
-    pass
+    rooms_list = input_data.splitlines()
+    pattern = r"([a-z\-]+)-(\d+)\[([a-z]+)\]"
+    output = []
+    for room in rooms_list:
+        matched_pattern = re.match(pattern, room)
+        decoded_room = [matched_pattern.group(1), int(matched_pattern.group(2)), matched_pattern.group(3)]
+        decoded_room[0] = decrypt_room_name(decoded_room[0], decoded_room[1])
+        if "north" in decoded_room[0]:
+            output += decoded_room
+    return output
 
 
 if __name__ == "__main__":
