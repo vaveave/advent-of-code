e9653815e5f9c3a4954dcdb0b6b012b526c1dc86
Valerio Avenoso <valerio.avenoso@gmail.com>
2024-11-18 01:19:09 +0100

    clean prints, remove redundant code

--- 2016/day_05.py
+++ 2016/day_05.py
@@ -1,36 +1,37 @@
 import hashlib
 from pathlib import Path
+import time
 
 
 def part1(input_data):
     print("Running part 1...")
     index = 0
     password = ""
-    while (len(password) <= 7):
+    while len(password) <= 7:
         door_id_plus_index = input_data + str(index)
         md5_hash = hashlib.md5(door_id_plus_index.encode()).hexdigest()
         if md5_hash[:5] == "00000":
             password += md5_hash[5]
+            print(f"Character {md5_hash[5]} added! Updated password: {password}")
         index += 1
-        if index % 1e5 == 0:
-            print(f"Total iterations: {str(index)}")
     return password
 
 
 def part2(input_data):
     index = 0
-    password = ["_"]*8
+    password = ["_"] * 8
     len_password = 0
-    while (len_password <= 7):
+    while len_password <= 7:
         door_id_plus_index = input_data + str(index)
         md5_hash = hashlib.md5(door_id_plus_index.encode()).hexdigest()
         if md5_hash[:5] == "00000":
             print(f"New possible hash: {md5_hash}")
-            ind = md5_hash[5]
+            pos = md5_hash[5]
             possible_char = md5_hash[6]
-            if ind.isdigit():
-                if  int(ind) <= 7 and password[int(ind)] == "_":
-                    password[int(ind)] = possible_char
+            if pos.isdigit():
+                pos = int(pos)
+                if pos <= 7 and password[pos] == "_":
+                    password[pos] = possible_char
                     len_password += 1
                     print(f"Hash is valid! Updated password: {''.join(password)}")
                 else:
@@ -38,10 +39,7 @@ def part2(input_data):
             else:
                 print("Hash not valid.")
         index += 1
-    if len(password) <= 7:
-        return f"password not found after {str(index)} iterations!"
-    else:
-        return "".join(password)
+    return "".join(password)
 
 
 if __name__ == "__main__":
@@ -50,5 +48,14 @@ if __name__ == "__main__":
     with open(input_file) as f:
         data = f.read().strip()
 
-    print("Part 1:", part1(data))
-    print("Part 2:", part2(data))
+    # Timing part 1
+    start_time = time.time()
+    result_part1 = part1(data)
+    end_time = time.time()
+    print(f"Part 1: {result_part1} (Time taken: {end_time - start_time:.2f} seconds)")
+
+    # Timing part 2
+    start_time = time.time()
+    result_part2 = part2(data)
+    end_time = time.time()
+    print(f"Part 2: {result_part2} (Time taken: {end_time - start_time:.2f} seconds)")
