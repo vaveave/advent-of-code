b6a7d1ec5f84cb116ddfc6f9abfcc5f5f50e0a63
Valerio Avenoso <valerio.avenoso@gmail.com>
2024-11-21 00:39:29 +0100

    boiler template approach

--- 2016/day_06.py
+++ 2016/day_06.py
@@ -1,22 +1,10 @@
-from pathlib import Path
 from collections import Counter
 
+input_file = "input.txt"
+with open(input_file) as f:
+    input_data = f.read().strip()
 
-def part1(input_data):
-    most_common_chr = [Counter(msg).most_common() for msg in zip(*input_data.splitlines())]
-    return "".join(x[0][0] for x in most_common_chr)
+most_common_chr = [Counter(msg).most_common() for msg in zip(*input_data.splitlines())]
 
-
-def part2(input_data):
-    most_common_chr = [Counter(msg).most_common() for msg in zip(*input_data.splitlines())]
-    return "".join(x[-1][0] for x in most_common_chr)
-
-
-if __name__ == "__main__":
-    # Get the input file path and read the input data
-    input_file = Path(__file__).parent / f"day_06_input.txt"
-    with open(input_file) as f:
-        data = f.read().strip()
-    
-    print("Part 1:", part1(data))
-    print("Part 2:", part2(data))
+print("Part 1 : " + "".join(x[0][0] for x in most_common_chr))
+print("Part 2 : " + "".join(x[-1][0] for x in most_common_chr))
