019d2ae888c752309452b70ab306a83e7cf88732
Valerio Avenoso <valerio.avenoso@gmail.com>
2024-11-23 16:05:11 +0100

    complete year 2016 - day 7 - part 2

--- aoc/2016/7/__main__.py
+++ aoc/2016/7/__main__.py
@@ -2,31 +2,60 @@ import re
 from pathlib import Path
 
 
-def is_abba(string):
-    if (string[0] == string[3]) and (string[1] == string[2]) and (string[0] != string[2]):
+def is_abba(segment):
+    if (
+            (segment[0] == segment[3])
+            and (segment[1] == segment[2])
+            and (segment[0] != segment[2])
+    ):
         return True
     return False
 
 
-def contain_abbas(string):
-    for i in range(4, len(string)+1):
-        if is_abba(string[i-4:i]):
+def is_aba(segment):
+    if (segment[0] == segment[2]) and (segment[0] != segment[1]):
+        return True
+    return False
+
+
+def is_bab(aba, hypernet):
+    a, b = aba[0], aba[1]
+    bab = f"{b}{a}{b}"
+    return bab in hypernet
+
+
+def contain_abbas(net):
+    for i in range(4, len(net) + 1):
+        if is_abba(net[i - 4:i]):
             return True
     return False
 
+
 def split_ip_components(ip):
     ip_components = re.split(r"[\[\]]+", ip)
-    return (ip_components[0::2], ip_components[1::2])
+    return ip_components[0::2], ip_components[1::2]
 
 
 def support_tls(ip):
-    supernet_sequences, hypernet_sequences = split_ip_components(ip)
-    if not any(map(contain_abbas, hypernet_sequences)) and any(map(contain_abbas, supernet_sequences)):
+    supernets, hypernets = split_ip_components(ip)
+    if (
+        not any(map(contain_abbas, hypernets))
+        and any(map(contain_abbas, supernets))
+    ):
         return True
     return False
 
+
 def support_ssl(ip):
-    pass
+    supernets, hypernets = split_ip_components(ip)
+    for supernet in supernets:
+        for i in range(3, len(supernet) + 1):
+            segment = supernet[i - 3:i]
+            if is_aba(segment):
+                if any(is_bab(segment, hypernet) for hypernet in hypernets):
+                    return True
+    return False
+
 
 def part_1(input_data):
     input_data = input_data.splitlines()
