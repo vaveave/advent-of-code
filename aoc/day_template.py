a76f9fbde485a26cd08f062c52e12d54e9ab7b77
vaveave <valerio.avenoso@gmail.com>
2024-12-01 23:00:46 +0100

    Add input parser to daily template

--- aoc/day_template.py
+++ aoc/day_template.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
 
+def read_input(input_data):
+    return input_data
+
+
 def part_1(input_data):
     # Implement part 1 solution
     pass
@@ -15,5 +19,5 @@ if __name__ == "__main__":
     with (Path(__file__).parent / "input.txt").open("r") as f:
         data = f.read()
 
-    print("Part 1:", part_1(data))
-    print("Part 2:", part_2(data))
+    print("Part 1:", part_1(read_input(data)))
+    print("Part 2:", part_2(read_input(data)))
