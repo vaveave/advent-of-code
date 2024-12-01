44e4466789c94a0a7dd83ebbbee8166aac961fdc
vaveave <valerio.avenoso@gmail.com>
2024-12-01 23:01:12 +0100

    Uniform to new daily template

--- aoc/2024/01/__main__.py
+++ aoc/2024/01/__main__.py
@@ -2,13 +2,17 @@ import numpy as np
 from pathlib import Path
 
 
-def part_1(input_data):
-    list_1, list_2 = -np.sort(-np.fromstring(input_data, sep=' ', dtype=int).reshape(-1, 2).T)
+def read_input(input_data):
+    return np.fromstring(input_data, sep=' ', dtype=int).reshape(-1, 2).T
+
+
+def part_1(location_ids):
+    list_1, list_2 = -np.sort(-location_ids)
     return np.sum(abs(list_2 - list_1))
 
 
-def part_2(input_data):
-    list_1, list_2 = np.fromstring(input_data, sep=' ', dtype=int).reshape(-1, 2).T
+def part_2(location_ids):
+    list_1, list_2 = location_ids
     return sum(location * np.count_nonzero(list_2 == location) for location in list_1)
 
 
@@ -24,5 +28,5 @@ if __name__ == "__main__":
     with (Path(__file__).parent / "input.txt").open("r") as f:
         data = f.read()
 
-    print("Part 1:", part_1(data))
-    print("Part 2:", part_2(data))
+    print("Part 1:", part_1(read_input(data)))
+    print("Part 2:", part_2(read_input(data)))
