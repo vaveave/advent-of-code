9465ff56e75fe70ff4d944dbbf2e237234869f6f
vaveave <valerio.avenoso@gmail.com>
2024-12-02 08:56:08 +0100

    Improve day 2, 2024

--- aoc/2024/02/__main__.py
+++ aoc/2024/02/__main__.py
@@ -1,19 +1,16 @@
+import numpy as np
 from pathlib import Path
 
 
 def read_input(input_data):
-    return [[int(entry) for entry in row.split(" ")] for row in input_data.splitlines()]
+    return [np.fromstring(row, sep=" ", dtype=int) for row in input_data.splitlines()]
 
 
 def is_report_safe(report):
-    is_asc = report[0] <= report[1]
-    for i in range(len(report)-1):
-        if is_asc:
-            if not (1 <= report[i+1] - report[i] <= 3):
-                return False
-        elif not (1 <= report[i] - report[i+1] <= 3):
-            return False
-    return True
+    report_diff = np.diff(report)
+    is_safely_increasing = np.all((report_diff >= 1) & (report_diff <= 3))
+    is_safely_decreasing = np.all((report_diff >= -3) & (report_diff <= -1))
+    return is_safely_increasing or is_safely_decreasing
 
 
 def part_1(reports):
@@ -22,21 +19,13 @@ def part_1(reports):
 
 def is_report_dampened_safe(report):
     for i in range(len(report)):
-        if is_report_safe([report[j] for j in range(len(report)) if j!=i]):
+        if is_report_safe(np.delete(report, i)):
             return True
     return False
 
 
 def part_2(reports):
     return sum([is_report_dampened_safe(report) for report in reports])
-
-
-test_data = """7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9"""
 
 
 if __name__ == "__main__":
