975c6ea8c447304af621a99f4c01466869302257
vaveave <valerio.avenoso@gmail.com>
2024-12-07 01:10:44 +0100

    Convert array to bool

--- aoc/2024/06/__main__.py
+++ aoc/2024/06/__main__.py
@@ -7,7 +7,7 @@ map_to_directions = {
     "<": (0, -1)
 }
 map_to_symbols = {tuple(value): key for key, value in map_to_directions.items()}
-
+char_to_bool = {'.': True, '^': True, '>': True, 'v': True, '<': True, '#': False}
 
 rotate = {
     (1, 0): (0, -1),
@@ -26,20 +26,23 @@ def read_input(input_data):
 
 
 def next_position_direction(grid, position, direction):
-    position = np.array(position)
-    direction = np.array(direction)
-    while grid[tuple(position + direction)] == "#":
-        direction = np.array(rotate[tuple(direction)])
-    new_position = position + direction
-    return tuple(new_position), tuple(direction)
+    pos_x, pos_y = position
+    dir_x, dir_y = direction
+
+    while not grid[pos_x + dir_x, pos_y + dir_y]:
+        dir_x, dir_y = rotate[(dir_x, dir_y)]  # Assuming `rotate` is a dictionary.
+
+    new_position = (pos_x + dir_x, pos_y + dir_y)
+    new_direction = (dir_x, dir_y)
+    return new_position, new_direction
 
 
-def is_bound_reached(grid, next_position):
+def is_bound_reached(next_position):
     is_outside = any([
         (next_position[0] == 0),
-        (next_position[0] == grid.shape[0]-1),
+        (next_position[0] == grid_size_0),
         (next_position[1] == 0),
-        (next_position[1] == grid.shape[1]-1)
+        (next_position[1] == grid_size_1)
     ])
     return is_outside
 
@@ -47,7 +50,7 @@ def is_bound_reached(grid, next_position):
 def follow_path(grid, hist_steps):
     hist_steps = hist_steps.copy()
     curr_position, curr_direction = hist_steps[-1]
-    while not is_bound_reached(grid, curr_position):
+    while not is_bound_reached(curr_position):
         next_step = next_position_direction(grid, curr_position, curr_direction)
         if next_step in set(hist_steps):
             raise InfiniteLoop
@@ -57,10 +60,11 @@ def follow_path(grid, hist_steps):
     return len(set(pos for pos, _ in hist_steps))
 
 
-def part_1(grid):
-    start_position = np.where(np.isin(grid, list(map_to_directions.keys())))
+def part_1(input_data):
+    start_position = np.where(np.isin(input_data, list(map_to_directions.keys())))
     start_position = tuple(p[0] for p in start_position)
-    start_direction = map_to_directions[grid[start_position]]
+    start_direction = map_to_directions[input_data[start_position]]
+    grid = np.vectorize(char_to_bool.get)(input_data)
     return follow_path(grid, [(start_position, start_direction)])
 
 
@@ -75,11 +79,11 @@ def try_path(grid, hist_steps):
 def follow_what_if_path(grid, hist_steps):
     curr_position, curr_direction = hist_steps[-1]
     obstacles_counter = 0
-    while not is_bound_reached(grid, curr_position):
+    while not is_bound_reached(curr_position):
         next_step = next_position_direction(grid, curr_position, curr_direction)
         # grid[next_step[0]] = "0"
         grid_to_be_tested = grid.copy()
-        grid_to_be_tested[next_step[0]] = "#"
+        grid_to_be_tested[next_step[0]] = False
         if try_path(grid_to_be_tested, hist_steps):
             obstacles_counter += 1
         curr_position, curr_direction = next_step
@@ -88,10 +92,11 @@ def follow_what_if_path(grid, hist_steps):
     return obstacles_counter
 
 
-def part_2(grid):
-    start_position = np.where(np.isin(grid, list(map_to_directions.keys())))
+def part_2(input_data):
+    start_position = np.where(np.isin(input_data, list(map_to_directions.keys())))
     start_position = tuple(p[0] for p in start_position)
-    start_direction = map_to_directions[grid[start_position]]
+    start_direction = map_to_directions[input_data[start_position]]
+    grid = np.vectorize(char_to_bool.get)(input_data)
     return follow_what_if_path(grid, [(start_position, start_direction)])
 
 
@@ -111,5 +116,8 @@ if __name__ == "__main__":
 
     from aoc.initialize_day import load_input
     data = load_input(__file__)
-    print("Part 1:", part_1(read_input(data)))
-    #print("Part 2:", part_2(read_input(data)))
+    grid_ = read_input(data)
+    grid_size_0 = grid_.shape[0]-1
+    grid_size_1 = grid_.shape[1]-1
+    print("Part 1:", part_1(grid_))
+    print("Part 2:", part_2(grid_))
