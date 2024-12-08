d3548b8df3395fdc9d9aa3930d5fa44e88c75167
vaveave <valerio.avenoso@gmail.com>
2024-12-08 18:55:34 +0100

    Improve day 8, 2024

--- aoc/2024/08/__main__.py
+++ aoc/2024/08/__main__.py
@@ -5,43 +5,36 @@ from itertools import combinations
 def read_input(input_data):
     grid = np.array([list(row) for row in input_data.splitlines()])
     unique_frequencies = np.unique(grid[grid != "."])
-    frequencies_locations = []
-    for freq in unique_frequencies:
-        frequencies_locations.append(np.argwhere(grid == freq))
-    return grid, unique_frequencies, frequencies_locations
+    frequencies_locations = [np.argwhere(grid == freq) for freq in unique_frequencies]
+    return grid, frequencies_locations
 
 
-def part_1(grid, unique_frequencies, frequencies_locations):
+def calculate_antinodes(grid, frequencies_locations, with_resonance=False):
     antinodes = np.zeros(grid.shape, dtype=bool)
-    for i, frequency in enumerate(unique_frequencies):
-        locations = frequencies_locations[i]
-        row_indices = list(combinations(range(locations.shape[0]), 2))
-        row_combinations = [(locations[i], locations[j]) for i, j in row_indices]
-        for combo in row_combinations:
+    for locations in frequencies_locations:
+        location_pairs = list(combinations(locations, 2))
+        for pair in location_pairs:
             for j in range(2):
-                node_x = 2 * combo[(j+1) % 2][0] - combo[j][0]
-                node_y = 2 * combo[(j+1) % 2][1] - combo[j][1]
-                if (0 <= node_x < grid.shape[0]) and (0 <= node_y < grid.shape[1]):
+                dx = pair[(j + 1) % 2][0] - pair[j][0]
+                dy = pair[(j + 1) % 2][1] - pair[j][1]
+                node_x = pair[(j + 1) % 2][0] + dx
+                node_y = pair[(j + 1) % 2][1] + dy
+                while (0 <= node_x < grid.shape[0]) and (0 <= node_y < grid.shape[1]):
                     antinodes[node_x, node_y] = True
+                    if not with_resonance:
+                        break
+                    node_x += dx
+                    node_y += dy
+    return antinodes
+
+
+def part_1(grid, frequencies_locations):
+    antinodes = calculate_antinodes(grid, frequencies_locations)
     return np.sum(np.sum(antinodes))
 
 
-def part_2(grid, unique_frequencies, frequencies_locations):
-    antinodes = np.zeros(grid.shape, dtype=bool)
-    for i, frequency in enumerate(unique_frequencies):
-        locations = frequencies_locations[i]
-        row_indices = list(combinations(range(locations.shape[0]), 2))
-        row_combinations = [(locations[i], locations[j]) for i, j in row_indices]
-        for combo in row_combinations:
-            for j in range(2):
-                dx = combo[(j+1) % 2][0] - combo[j][0]
-                dy = combo[(j+1) % 2][1] - combo[j][1]
-                node_x = combo[(j+1) % 2][0] + dx
-                node_y = combo[(j+1) % 2][1] + dy
-                while (0 <= node_x < grid.shape[0]) and (0 <= node_y < grid.shape[1]):
-                    antinodes[node_x, node_y] = True
-                    node_x += dx
-                    node_y += dy
+def part_2(grid, frequencies_locations):
+    antinodes = calculate_antinodes(grid, frequencies_locations, with_resonance=True)
     grid[antinodes] = "#"
     return np.sum(np.sum(grid != "."))
 
@@ -50,7 +43,7 @@ if __name__ == "__main__":
 
     from aoc.initialize_day import load_input
     data = load_input(__file__)
-    grid_, unique_frequencies_, frequencies_locations_ = read_input(data)
+    grid_, frequencies_locations_ = read_input(data)
 
-    print("Part 1:", part_1(grid_, unique_frequencies_, frequencies_locations_))
-    print("Part 2:", part_2(grid_, unique_frequencies_, frequencies_locations_))
+    print("Part 1:", part_1(grid_, frequencies_locations_))
+    print("Part 2:", part_2(grid_, frequencies_locations_))
