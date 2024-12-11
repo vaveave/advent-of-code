93514b5d6756bf4fee32ebf362989822aa2cc9cb
vaveave <valerio.avenoso@gmail.com>
2024-12-11 07:29:34 +0100

    Solve day 11, 2024, part 1

---
+++ aoc/2024/11/__main__.py
@@ -0,0 +1,38 @@
+from itertools import chain
+
+
+def read_input(input_data):
+    return input_data.split(" ")
+
+
+def transform_rules(stone: str):
+    if stone == "0":
+        return ["1"]
+    elif (len(stone) % 2) == 0:
+        new_len = int(len(stone)/2)
+        return [str(int(stone[:new_len])), str(int(stone[new_len:]))]
+    else:
+        return [str(int(stone) * 2024)]
+
+
+def part_1(input_data, blinks):
+    stones_evolutions = [[]]*(blinks+1)
+    stones_evolutions[0] = input_data
+    for blink in range(1, blinks+1):
+        stones_evolutions[blink] = list(
+            chain.from_iterable([transform_rules(stone) for stone in stones_evolutions[blink-1]])
+        )
+    return len(stones_evolutions[-1])
+
+
+def part_2(input_data):
+    # Implement part 2 solution
+    pass
+
+
+if __name__ == "__main__":
+
+    from aoc.initialize_day import load_input
+    data = load_input(__file__)
+    print("Part 1:", part_1(read_input(data), 25))
+    # print("Part 2:", part_1(read_input(data), 75))
