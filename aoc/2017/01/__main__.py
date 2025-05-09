e15902ff9d417472c98017f87d5fe58f84acdb8d
vaveave <valerio.avenoso@gmail.com>
2025-05-10 01:48:57 +0200

    Refactor calculation

--- aoc/2017/01/__main__.py
+++ aoc/2017/01/__main__.py
@@ -1,29 +1,19 @@
-
 def read_input(input_data):
     return input_data
 
 
-def part_1(input_data):
-    counter = 0
-    input_data += input_data[0]
-    for i in range(len(input_data)-1):
-        if input_data[i] == input_data[i+1]:
-            counter += int(input_data[i])
-    return counter
-
-
-def part_2(input_data):
-    counter = 0
+def calculate_sum(input_data, offset):
     length = len(input_data)
-    for i in range(length):
-        if input_data[i] == input_data[(length//2 + i) % length]:
-            counter += int(input_data[i])
-    return counter
+    return sum(
+        int(input_data[i]) 
+        for i in range(length) 
+        if input_data[i] == input_data[(i + offset) % length]
+    )
 
 
 if __name__ == "__main__":
 
     from aoc.initialize_day import load_input
     data = load_input(__file__)
-    print("Part 1:", part_1(read_input(data)))
-    print("Part 2:", part_2(read_input(data)))
+    print("Part 1:", calculate_sum(data, 1))  # Offset of 1 for part 1
+    print("Part 2:", calculate_sum(data, len(data) // 2))  # Offset of length//2 for part 2
