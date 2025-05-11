740987405accdc6420c41b2d1f8c2ecbc56ed461
vaveave <valerio.avenoso@gmail.com>
2025-05-11 03:00:03 +0200

    Solve day 3, 2017, part 1

---
+++ aoc/2017/03/__main__.py
@@ -0,0 +1,32 @@
+def read_input(input_data):
+    return int(input_data)
+
+
+def part_1(input_data):
+    x, y = 0, 0
+    dx, dy = 0, -1
+    history = {(0, 0)}
+    for _ in range(input_data - 1):
+        dx_new, dy_new = -dy, dx
+        x_new = x + dx_new
+        y_new = y + dy_new
+        if (x_new, y_new) not in history:
+            x, y = x_new, y_new
+            dx, dy = dx_new, dy_new
+        else:
+            x += dx
+            y += dy
+        history.add((x, y))
+    return abs(x) + abs(y)
+
+
+def part_2(input_data):
+    # Implement part 2 solution
+    pass
+
+
+if __name__ == "__main__":
+    from aoc.initialize_day import load_input
+    data = load_input(__file__)
+    print("Part 1:", part_1(read_input(data)))
+    print("Part 2:", part_2(read_input(data)))
