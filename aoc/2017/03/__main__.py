b344dda2e32972338ece181264f8f2d6ad9aa318
vaveave <valerio.avenoso@gmail.com>
2025-05-17 23:16:36 +0200

    Refactor day 3, 2017

--- aoc/2017/03/__main__.py
+++ aoc/2017/03/__main__.py
@@ -2,18 +2,23 @@ def read_input(input_data):
     return int(input_data)
 
 
+def spiral_move(x, y, dx, dy, visited):
+    dx_new, dy_new = -dy, dx
+    x_new, y_new = x + dx_new, y + dy_new
+    if (x_new, y_new) not in visited:
+        x, y = x_new, y_new
+        dx, dy = dx_new, dy_new
+    else:
+        x, y = x + dx, y + dy
+    return x, y, dx, dy
+
+
 def part_1(input_data):
     x, y = 0, 0
     dx, dy = 0, -1
     visited = {(0, 0)}
     for _ in range(input_data - 1):
-        dx_new, dy_new = -dy, dx
-        x_new, y_new = x + dx_new, y + dy_new
-        if (x_new, y_new) not in visited:
-            x, y = x_new, y_new
-            dx, dy = dx_new, dy_new
-        else:
-            x, y = x + dx, y + dy
+        x, y, dx, dy = spiral_move(x, y, dx, dy, visited)
         visited.add((x, y))
     return abs(x) + abs(y)
 
@@ -22,18 +27,17 @@ def part_2(input_data):
     x, y = 0, 0
     dx, dy = 0, -1
     visited = {(0, 0): 1}
-    neighbors = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    neighbors = [
+        (0, 1), (1, 0), (0, -1), (-1, 0), 
+        (1, 1), (1, -1), (-1, 1), (-1, -1)
+    ]
     while True:
         if visited[(x, y)] > input_data:
             return visited[(x, y)]
-        dx_new, dy_new = -dy, dx
-        x_new, y_new = x + dx_new, y + dy_new
-        if (x_new, y_new) not in visited:
-            x, y = x_new, y_new
-            dx, dy = dx_new, dy_new
-        else:
-            x, y = x + dx, y + dy
-        visited[(x, y)] = sum(visited.get((x + dx, y + dy), 0) for dx, dy in neighbors)
+        x, y, dx, dy = spiral_move(x, y, dx, dy, visited)
+        visited[(x, y)] = sum(
+            visited.get((x + dx, y + dy), 0) for dx, dy in neighbors
+        )
 
 
 if __name__ == "__main__":
