3969b532544fd2393e11b0a4977b4ef907f7c955
vaveave <valerio.avenoso@gmail.com>
2025-05-18 21:16:40 +0200

    Refactor day 6, 2017

--- aoc/2017/06/__main__.py
+++ aoc/2017/06/__main__.py
@@ -5,34 +5,31 @@ def read_input(input_data):
     return [int(line) for line in input_data.split("\t")]
 
 
-def part_1_and_2(all_blocks: list[int]):
-    length = len(all_blocks)
-    cnt = 0
-    seen = {}
+def redistribute_blocks(memory_banks: list[int]) -> tuple[int, int]:
+    num_banks = len(memory_banks)
+    cycle_count = 0
+    seen_configs = {}
     while True:
-        cnt += 1
-        seen[tuple(all_blocks)] = cnt
-        # Find the index of the largest block
-        argmax, max_val = max(enumerate(all_blocks), key=lambda x: x[1])
-        # Distribute blocks
-        blocks_to_distribute = ceil(max_val / length)
-        distributed = max_val
-        for i in range(length - 1):
-            ind = (argmax + i + 1) % length
-            new_val = min(blocks_to_distribute, distributed)
-            # Distribute blocks to the next index
-            all_blocks[ind] += new_val
-            distributed -= new_val
-        all_blocks[argmax] = distributed
-        if tuple(all_blocks) in seen:
+        cycle_count += 1
+        seen_configs[tuple(memory_banks)] = cycle_count
+        max_index, blocks = max(enumerate(memory_banks), key=lambda x: x[1])
+        base_blocks = ceil(blocks / num_banks)
+        remaining_blocks = blocks
+        for i in range(num_banks - 1):
+            target_index = (max_index + i + 1) % num_banks
+            give = min(base_blocks, remaining_blocks)
+            memory_banks[target_index] += give
+            remaining_blocks -= give
+        memory_banks[max_index] = remaining_blocks
+        if tuple(memory_banks) in seen_configs:
             break
-    return cnt, cnt - seen[tuple(all_blocks)] + 1
+    return cycle_count, cycle_count - seen_configs[tuple(memory_banks)] + 1
 
 
 if __name__ == "__main__":
     from aoc.initialize_day import load_input
 
     data = load_input(__file__)
-    cnt, cycle_length = part_1_and_2(read_input(data))
-    print("Part 1:", cnt)
-    print("Part 2:", cycle_length)
+    total_cycles, loop_size = redistribute_blocks(read_input(data))
+    print("Part 1:", total_cycles)
+    print("Part 2:", loop_size)
