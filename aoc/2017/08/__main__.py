e1ca01325b7d663be0c79ee5459620a44451d397
vaveave <valerio.avenoso@gmail.com>
2025-08-31 00:17:11 +0200

    Improve day 8, 2017

--- aoc/2017/08/__main__.py
+++ aoc/2017/08/__main__.py
@@ -2,28 +2,42 @@ def read_input(input_data):
     return [line.split() for line in input_data.splitlines()]
 
 
-def part_1_2(input_data):
-    # Values
-    max_reached = 0
-    registers = {instr[0] for instr in input_data}
-    [exec(register + "=0") for register in registers]
-    for instr in input_data:
-        # Example line: ['d', 'dec', '683', 'if', 'qn', '==', '0']
-        condition = "".join(instr[4:])
-        if eval(condition):
-            if instr[1] == "inc":
-                exec(instr[0] + "=" + instr[0] + "+" + instr[2])
-            else:  # instr[1] == "dec":
-                exec(instr[0] + "=" + instr[0] + "-" + instr[2])
-        max_reached_instr = f"max({','.join([instr[0] for instr in input_data])})"
-        max_reached = eval(f"max(max_reached, {max_reached_instr})")
-    return eval(max_reached_instr), max_reached
+def part_1_and_2(instructions):
+    registers = {}
+    max_reached = float('-inf')
+
+    # Map of condition operators to actual Python lambdas
+    ops = {
+        "==": lambda x, y: x == y,
+        "!=": lambda x, y: x != y,
+        "<" : lambda x, y: x < y,
+        "<=": lambda x, y: x <= y,
+        ">" : lambda x, y: x > y,
+        ">=": lambda x, y: x >= y
+    }
+
+    for reg, action, value, _, cond_reg, cond_op, cond_value in instructions:
+        value = int(value)
+        cond_value = int(cond_value)
+
+        # Initialize registers if not seen yet
+        registers.setdefault(reg, 0)
+        registers.setdefault(cond_reg, 0)
+
+        # Check condition
+        if ops[cond_op](registers[cond_reg], cond_value):
+            registers[reg] += value if action == "inc" else -value
+
+        # Track max value ever held in any register
+        max_reached = max(max_reached, max(registers.values()))
+
+    return max(registers.values()), max_reached
 
 
 if __name__ == "__main__":
     from aoc.initialize_day import load_input
 
     data = load_input(__file__)
-    last_max, max_reached = part_1_2(read_input(data))
-    print("Part 1:", last_max)
-    print("Part 2:", max_reached)
+    part_1, part_2 = part_1_and_2(read_input(data))
+    print("Part 1:", part_1)
+    print("Part 2:", part_2)
