0868ddf66cbc96d8b24346342b084f42ded76052
vaveave <valerio.avenoso@gmail.com>
2025-08-31 01:55:39 +0200

    Solve day 9, 2017, part 2 and simplify code

--- aoc/2017/09/__main__.py
+++ aoc/2017/09/__main__.py
@@ -1,36 +1,32 @@
-def clean_data(input_data):
-    cleaned_data = ""
-    ind = 0
-    while ind < len(input_data):
-        if input_data[ind] == "!":
-            ind += 2
+import re
+
+from aoc.initialize_day import load_input
+
+
+def clean_data(data: str) -> str:
+    # Remove canceled characters: any "!" followed by one character
+    return re.sub(r"!.", "", data)
+
+
+def part_1_and_2(data):
+    data = clean_data(data)
+    score = depth = garbage = ind = 0
+    while ind < len(data):
+        if data[ind] == "<":
+            new_garbage = data[ind:].find(">")
+            ind += new_garbage + 1
+            garbage += new_garbage - 1
             continue
-        cleaned_data += input_data[ind]
+        if data[ind] == "{":
+            depth += 1
+        elif data[ind] == "}":
+            score += depth
+            depth -= 1
         ind += 1
-    return cleaned_data
+    return score, garbage
 
 
-def part_1_and_2(cleaned_data):
-    score = 0
-    open_groups = 0
-    ind = 0
-    while ind < len(cleaned_data):
-        if cleaned_data[ind] == "<":
-            ind += cleaned_data[ind:].find(">") + 1
-            continue
-        if cleaned_data[ind] == "{":
-            open_groups += 1
-        elif cleaned_data[ind] == "}":
-            score += open_groups
-            open_groups -= 1
-        ind += 1
-    return score, None
-
-
-if __name__ == "__main__":
-    from aoc.initialize_day import load_input
-
-    data = load_input(__file__)
-    part_1, part_2 = part_1_and_2(clean_data(data))
-    print("Part 1:", part_1)
-    print("Part 2:", part_2)
+input_data = load_input(__file__)
+part_1, part_2 = part_1_and_2(input_data)
+print("Part 1:", part_1)
+print("Part 2:", part_2)
