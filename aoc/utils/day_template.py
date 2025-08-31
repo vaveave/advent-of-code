8110728796fbb97556830723e732d755b397711e
vaveave <valerio.avenoso@gmail.com>
2025-08-31 13:16:34 +0200

    Move initialization and template logic to utils folder

---
+++ aoc/utils/day_template.py
@@ -0,0 +1,20 @@
+TEMPLATE = """from aoc.utils import load_input
+
+
+def read_input(data):
+    return data
+
+
+def part_1(data):
+    # Implement part 1 solution
+    pass
+
+
+def part_2(data):
+    # Implement part 2 solution
+    pass
+
+input_data = load_input(__file__)
+print("Part 1:", part_1(read_input(input_data)))
+print("Part 2:", part_2(read_input(input_data)))
+"""
