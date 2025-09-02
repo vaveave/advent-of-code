7db37f103539c67ab4bcf294d9523dffad0283f9
vaveave <valerio.avenoso@gmail.com>
2025-09-03 00:14:27 +0200

    Add back  day 9, 2017

---
+++ aoc/solutions/2017/09/__main__.py
@@ -0,0 +1,37 @@
+import re
+
+from aoc.cli.utils import load_input
+
+
+def clean_data(data: str) -> str:
+    # Remove canceled characters: any "!" followed by one character
+    return re.sub(r"!.", "", data)
+
+
+def part_1_and_2(data):
+    data = clean_data(data)
+    score = depth = garbage = ind = 0
+    while ind < len(data):
+        if data[ind] == "<":
+            new_garbage = data[ind:].find(">")
+            ind += new_garbage + 1
+            garbage += new_garbage - 1
+            continue
+        if data[ind] == "{":
+            depth += 1
+        elif data[ind] == "}":
+            score += depth
+            depth -= 1
+        ind += 1
+    return score, garbage
+
+
+def main():
+    data = load_input(__file__)
+    part_1, part_2 = part_1_and_2(data)
+    print("Part 1:", part_1)
+    print("Part 2:", part_2)
+
+
+if __name__ == "__main__":
+    main()
